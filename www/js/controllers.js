99c2f3a00d9489c35422cc44e87b6898d3c268d1
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2015-04-26 22:48:39 +0300

    current time updating

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,13 @@
 angular.module('starter.controllers', [])
 
-    .controller('TimerCtrl', function ($scope) {
+    .controller('TimerCtrl', function ($scope, $interval) {
+
+        $scope.currentTime = new Date();
+
+        $interval(function() {
+            $scope.currentTime = new Date();
+        }, 70)
+
     })
 
     .controller('AccountCtrl', function ($scope) {
