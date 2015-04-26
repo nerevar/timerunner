6884b4c18f1155587a1e9202d195f0dc807ace83
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2015-04-26 21:05:37 +0300

    indenting

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,10 @@
 angular.module('starter.controllers', [])
 
-.controller('TimerCtrl', function($scope) {})
+    .controller('TimerCtrl', function ($scope) {
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
